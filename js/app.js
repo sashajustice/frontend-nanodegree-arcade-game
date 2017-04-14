32ad933ab45829beb5b9756b05c1d86596716c2f
sashajustice <sabrinnasreddin@gmail.com>
2017-04-14 15:07:48 -0700

    add all code for frogger

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+let Enemy = function() {
+  this.location = location
+  this.speed = speed
+  this.n = n
+  this.x = x
+
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -10,7 +15,22 @@ var Enemy = function() {
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+
+  update(dt) {
+    if(this.n =1000) {
+      this.x += this.speed * (dt)
+    } else {
+      this.reset()
+    }
+
+  }
+  reset() {
+    ctx.drawImage(Resources.get(this.sprite),
+      this.x, this.y
+    }
+  }
+Enemy.prototype.update = function(dt) { //update method
+
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -24,15 +44,92 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+function Player() {
+  this.update = update(),
+  this.render = render(),
+  player.location
+  prototype.handleInput
+  handleInput()
+}
+Player.prototype = function () {
+
+
+
+let allEnemies = [];
+
+}
+let enemy = new Enemy(200, 210, 225) // fifth
+allEnemies.push(enemy)
+enemy = new Enemy (20, 190, 150) // fourth
+allEnemies.push(enemy)
+enemy = new Enemy (150, 130, 300) // second
+allEnemies.push(enemy)
+enemy = new Enemy (10, 75, 85) // first
+allEnemies.push(enemy)
+
+class Player {
+    constructor(x, y, speed) {
+        this.x = x
+        this.y = y
+        this.speed = speed
+        this.sprite = 'images/char-slimer.png'
+    }
+    update() {
+        for (let i = 0; i < allEnemies.length; i++) {
+            if (Math.abs(this.x - allEnemies[i].x) <= 75) {
+                if (Math.abs(this.y - allEnemies[i].y) <= 75) {
+                    this.reset()
+                }
+            }
+
+       }
+        if ((this.y - 0) <= 5) {
+            this.reset()
+        }
+    }
+    reset() {
+        this.x = 200
+        this.y = 400
+    }
+    render() {
+    ctx.drawImage(Resources.get(this.sprite),
+        this.x, this.y)
+    }
+    handleInput(e) {
+        if (e === 'left' && this.x > 0) {
+            this.x -= 50
+        }
+        if (e === 'up' && this.y > 0) {
+            this.y -= 50
+        }
+        if (e === 'right' && this.x < 400) {
+            this.x += 50
+        }
+        if (e === 'down' && this.y < 400) {
+            this.y += 50
+        }
+
+   }
+
+}
+
+let player = new Player(200, 400, 10)
+
+
+
+
+
+  this.sprite = 'images/enemy-bug.png';
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var Egor = new allEnemies[]
+var player = new Player();
 
-
-
-// This listens for key presses and sends the keys to your
+// This listens for key pre
+sses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
@@ -43,4 +140,5 @@ document.addEventListener('keyup', function(e) {
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
+
 });
